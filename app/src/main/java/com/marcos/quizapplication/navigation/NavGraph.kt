4196549115b018867b4b92219787f936e6e3b344
Marcos Antônio <marcosjunior.me.009@gmail.com>
2025-08-15 21:42:52 -0300

    feat: adicinado navegação com Graph

---
+++ app/src/main/java/com/marcos/quizapplication/navigation/NavGraph.kt
@@ -0,0 +1,78 @@
+package com.marcos.quizapplication.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.marcos.quizapplication.ui.screens.HomeScreen
+import com.marcos.quizapplication.ui.screens.LoginScreen
+import com.marcos.quizapplication.ui.screens.QuizRoute
+import com.marcos.quizapplication.ui.viewmodel.HomeViewModel
+import com.marcos.quizapplication.ui.viewmodel.LoginViewModel
+import com.marcos.quizapplication.ui.viewmodel.QuizViewModel
+
+sealed class Screen(val route: String) {
+    object Login : Screen("login_screen")
+    object Home : Screen("home_screen")
+    object Quiz : Screen("quiz_screen/{quizId}") {
+        fun createRoute(quizId: String) = "quiz_screen/$quizId"
+    }
+}
+
+@Composable
+fun NavGraph(navController: NavHostController, startDestination: String) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination
+    ) {
+        composable(route = Screen.Login.route) {
+            val loginViewModel: LoginViewModel = hiltViewModel()
+            val uiState by loginViewModel.uiState.collectAsStateWithLifecycle()
+
+            LoginScreen(
+                uiState = uiState,
+                onSignInClick = loginViewModel::signIn,
+                onErrorMessageShown = loginViewModel::onErrorMessageShown,
+                onLoginSuccess = {
+                    navController.navigate(Screen.Home.route) {
+                        popUpTo(navController.graph.startDestinationId) { inclusive = true }
+                    }
+                }
+            )
+        }
+
+        composable(route = Screen.Home.route) {
+            val homeViewModel: HomeViewModel = hiltViewModel()
+            val uiState by homeViewModel.uiState.collectAsStateWithLifecycle()
+
+            HomeScreen(
+                userName = uiState.userName,
+                onLogout = {
+                    homeViewModel.onLogout()
+                    navController.navigate(Screen.Login.route) {
+                        popUpTo(navController.graph.startDestinationId) { inclusive = true }
+                    }
+                },
+                onStartQuizClick = { quizId ->
+                    navController.navigate(Screen.Quiz.createRoute(quizId))
+                }
+            )
+        }
+
+        composable(route = Screen.Quiz.route) {
+            val quizViewModel: QuizViewModel = hiltViewModel()
+            QuizRoute(
+                viewModel = quizViewModel,
+                onNavigateBack = { navController.popBackStack() },
+                onNavigateHome = {
+                    navController.navigate(Screen.Home.route) {
+                        popUpTo(Screen.Home.route) { inclusive = true }
+                    }
+                }
+            )
+        }
+    }
+}
